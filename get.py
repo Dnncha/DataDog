e78f6912ea1a039c55ac8b3506c3a37b803ab5ba
Thomas <northeyt@live.ie>
2016-02-02 15:42:03 +0000

    added my authtoken to give >50 API_calls/day

--- get.py
+++ get.py
@@ -2,7 +2,8 @@ import Quandl
 import numpy
 import pandas 
 
-data=Quandl.get("BCHARTS/BTCNCNY")
+# authtoken from tnorthey Quandl account allows >50 API pulls/day
+data=Quandl.get("BCHARTS/BTCNCNY", authtoken="_opb5TgvB6AbCAsFB7XS")
 
 print data
 
