8e06d5b1523380f482808401a08219812dae04e2
Thomas <northeyt@live.ie>
2016-02-02 19:30:20 +0000

    made get_data function

--- get.py
+++
@@ -1,9 +0,0 @@
-import Quandl
-import numpy
-import pandas 
-
-# authtoken from tnorthey Quandl account allows >50 API pulls/day
-data=Quandl.get("BCHARTS/BTCNCNY", authtoken="_opb5TgvB6AbCAsFB7XS")
-
-print data
-
